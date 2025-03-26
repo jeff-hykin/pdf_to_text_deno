557b94fd97fe2ec9c9bcf886d35cf6c2dd14b62c
Jeff Hykin <jeff.hykin@gmail.com>
2025-03-26 18:50:09 -0500

    add readme and stuff

--- main.js
+++ main.js
@@ -6,11 +6,12 @@ import { serve } from "https://deno.land/std@0.224.0/http/server.ts"
 
 import uint8ArrayForTestPdfPdf from "./test_data/test_pdf.pdf.binaryified.js"
 
-export class PdfToText {
+export class PdfToTextConverter {
     constructor({port=6060, hostIp, shouldWarn = true} = {}) {
         // spinup server and browser
         this.port = port
         this.shouldWarn = shouldWarn
+        this.runningConversion = Promise.resolve()
         
         // NOTE: I should be able to do it without a server, but for some reason astral doesn't work with a file://
         // import { pathToFileURL } from 'node:url'
@@ -65,36 +66,40 @@ export class PdfToText {
     }
     
     async convert(pdfData, {tryCap = 10,} = {}) {
-        if (!(pdfData instanceof Uint8Array)) {
-            throw new Error(`pdfData must be a Uint8Array`)
-        }
-        this.stringForPdfExtractionHtml = stringForIndexBundledHtml.replace(/PDF_UINT8_ARRAY_\$8539084 = new Uint8Array\(\[\]\)/, `PDF_UINT8_ARRAY_$8539084 = new Uint8Array([${uint8ArrayForTestPdfPdf}])`)
-        const page = await this.pagePromise
-        var value
-        let tries = 0
-        while (value == null) {
-            tries++
-            value = await page.evaluate(() => {
-                return {
-                    value: JSON.parse(globalThis.pdfTextContents||'null'),
-                    error: globalThis.pdfError,
-                    promiseResoved: globalThis.promiseResoved,
+        await this.runningConversion // wait for previous conversion to finish (otherwise we'll have problems)
+                                     // NOTE: if converting a ton of pdfs, this could definitely be optimized better to run them in parallel
+        return this.runningConversion = ((async ()=>{
+            if (!(pdfData instanceof Uint8Array)) {
+                throw new Error(`pdfData must be a Uint8Array`)
+            }
+            this.stringForPdfExtractionHtml = stringForIndexBundledHtml.replace(/PDF_UINT8_ARRAY_\$8539084 = new Uint8Array\(\[\]\)/, `PDF_UINT8_ARRAY_$8539084 = new Uint8Array([${uint8ArrayForTestPdfPdf}])`)
+            const page = await this.pagePromise
+            var value
+            let tries = 0
+            while (value == null) {
+                tries++
+                value = await page.evaluate(() => {
+                    return {
+                        value: JSON.parse(globalThis.pdfTextContents||'null'),
+                        error: globalThis.pdfError,
+                        promiseResoved: globalThis.promiseResoved,
+                    }
+                })
+                if (value.error) {
+                    throw new Error(value.error)
                 }
-            })
-            if (value.error) {
-                throw new Error(value.error)
-            }
-            if (value.promiseResoved) {
-                return value.value
-            }
-            if (!value.promiseResoved) {
-                if (tries > tryCap) {
-                    throw Error(`giving up on getting data out of browser for pdfToText, tried ${tryCap} times`)
-                    break
+                if (value.promiseResoved) {
+                    return value.value
+                }
+                if (!value.promiseResoved) {
+                    if (tries > tryCap) {
+                        throw Error(`giving up on getting data out of browser for pdfToText, tried ${tryCap} times`)
+                        break
+                    }
                 }
             }
-        }
-        return value
+            return value
+        })())
     }
     
     close() {
@@ -103,9 +108,9 @@ export class PdfToText {
     }
 }
 
-export async function pdfToText({ pdfData }) {
-    const pdfToText = new PdfToText()
-    const result = await pdfToText.convert(pdfData)
+export async function pdfToText(pdfData, {tryCap = 10,} = {}) {
+    const pdfToText = new PdfToTextConverter()
+    const result = await pdfToText.convert(pdfData, {tryCap})
     await pdfToText.close()
     return result
 }
---
+++ readme.md
@@ -0,0 +1,59 @@
+## What is this?
+
+This is a deno module that uses [astral](https://github.com/astral-sh/astral) and FireFox's [PDF.js](https://github.com/mozilla/pdf.js) to extract text from a PDF.
+
+## How do I use this?
+
+```js
+import { pdfToText, PdfToTextConverter } from "https://esm.sh/gh/jeff-hykin/pdf_to_text_deno@0.1.0/main.js"
+ 
+// if doing multiple conversions, use this:
+const converter = new PdfToTextConverter()
+const result = await converter.convert(pdfData, {tryCap})
+await pdfToText.close() // clean up resouces
+
+// if doing a one-off convert use this:
+const textObjects = await pdfToText(pdfData)
+textObjects == [
+  {
+    items: [
+      {
+        str: "Weather data is not",
+        dir: "ltr",
+        width: 47.423211000000016,
+        height: 5.115,
+        transform: [ 5.115, 0, 0, 5.115, 63.311, 769.2305 ],
+        fontName: "g_d0_f1",
+        hasEOL: false
+      },
+      {
+        str: "",
+        dir: "ltr",
+        width: 0,
+        height: 0,
+        transform: [ 5.115, 0, 0, 5.115, 63.311, 763.0305 ],
+        fontName: "g_d0_f1",
+        hasEOL: true
+      },
+      {
+        str: "available right now.",
+        dir: "ltr",
+        width: 46.56747150000002,
+        height: 5.115,
+        transform: [ 5.115, 0, 0, 5.115, 63.311, 763.0305 ],
+        fontName: "g_d0_f1",
+        hasEOL: false
+      }
+    ],
+    styles: {
+      g_d0_f1: {
+        fontFamily: "sans-serif",
+        ascent: 0.966796875,
+        descent: -0.2109375,
+        vertical: false
+      }
+    },
+    lang: null
+  }
+]
+```
