85b299a8bf3c7bc696f6368766ec7de4797fc693
Jeff Hykin <jeff.hykin@gmail.com>
2025-03-26 18:36:46 -0500

    clean

--- main.js
+++ main.js
@@ -65,7 +65,9 @@ export class PdfToText {
     }
     
     async convert(pdfData, {tryCap = 10,} = {}) {
-        pdfData = pdfData || uint8ArrayForTestPdfPdf
+        if (!(pdfData instanceof Uint8Array)) {
+            throw new Error(`pdfData must be a Uint8Array`)
+        }
         this.stringForPdfExtractionHtml = stringForIndexBundledHtml.replace(/PDF_UINT8_ARRAY_\$8539084 = new Uint8Array\(\[\]\)/, `PDF_UINT8_ARRAY_$8539084 = new Uint8Array([${uint8ArrayForTestPdfPdf}])`)
         const page = await this.pagePromise
         var value
